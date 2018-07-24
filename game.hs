6d24bd9a34111ffcf7aa7fb8ac5c96a8118b1768
Felipe Luzzardi da Rosa <fldrosa@inf.ufpel.edu.br>
2018-07-23 23:02:28 -0300

    Game finalized

--- game.hs
+++ game.hs
@@ -14,9 +14,9 @@ game b = if (isKingInCheckMate b True)
             else if (isKingInCheckMate b False)
                     then whiteWin
                     else do 
-                        nextWhiteMove b
-                        nextBlackMove b
-                        game b
+                        whiteBoard <- nextWhiteMove b
+                        blackBoard <- nextBlackMove whiteBoard
+                        game blackBoard
 
 blackWin :: IO ()
 blackWin = putStrLn "Xeque Mate! O jogador preto ganhou!"
@@ -24,42 +24,51 @@ blackWin = putStrLn "Xeque Mate! O jogador preto ganhou!"
 whiteWin :: IO ()
 whiteWin = putStrLn "Xeque Mate! O jogador branco ganhou!"
 
-nextWhiteMove :: Board -> IO ()
+nextWhiteMove :: Board -> IO Board
 nextWhiteMove b = do 
     putStrLn "Jogador branco, é sua vez"
-    move <- getMove b True
-    return ()
+    newBoard <- getMove b True
+    return newBoard
 
-nextBlackMove :: Board -> IO ()
+nextBlackMove :: Board -> IO Board
 nextBlackMove b = do
     putStrLn "Jogador preto, é a sua vez"
-    move <- getMove b False
-    return ()
+    newBoard <- getMove b False
+    return newBoard
 
-getMove :: Board -> Bool -> IO String
+getMove :: Board -> Bool -> IO Board
 getMove b c = do
     putStrLn "\tInsira sua jogada e tecle enter"
     move <- getLine
-    makeMove b c (convertMove board move)
-    return ""
+    newBoard <- makeMove b c (convertMove board move)
+    return newBoard
 
 convertMove :: Board -> String -> ((Char, Int),(Char,Int))
 convertMove board (a:b:c:d:e:f) = ((a,(digitToInt b)),(d,(digitToInt e)))
 
-makeMove :: Board -> Bool -> ((Char, Int),(Char,Int)) -> IO ()
+makeMove :: Board -> Bool -> ((Char, Int),(Char,Int)) -> IO Board
 makeMove b c ((oc,ol),(dc,dl)) = do 
     let piece = (findPiece b (oc,ol))
     if ((piece == (King c)) || (piece == (Queen c)) || (piece == (Bishop c)) || (piece == (Knight c)) || (piece == (Rook c)) || (piece == (Pawn c))) 
         && (isValidMove b piece (translateLine(oc),ol) (translateLine(dc),dl))
         && ((findPiece b (dc,dl) /= (King c)) && (findPiece b (dc,dl) /= (Queen c)) && (findPiece b (dc,dl) /= (Bishop c)) && (findPiece b (dc,dl) /= (Knight c)) && (findPiece b (dc,dl) /= (Rook c)) && (findPiece b (dc,dl) /= (Pawn c)))
         then do
-            putStrLn (printBoard 8 (movePiece b ((oc,ol),(dc,dl))))
+            let newBoard = (movePiece b ((oc,ol),(dc,dl)))
+            putStrLn (printBoard 8 newBoard)
             if (isKingInCheck b False 1 1)
-                then putStrLn "Xeque!"
-                else putStrLn ""
+                then do
+                    putStrLn "Xeque!"
+                    return newBoard
+                else do
+                    putStrLn ""
+                    return newBoard
         else do
             putStrLn "\nMovimento Inválido, tente novamente\n"
             if c
-                then nextWhiteMove b
-                else nextBlackMove b 
-    return ()
+                then do
+                    newBoard <- nextWhiteMove b
+                    return newBoard
+                else do
+                    newBoard <- nextBlackMove b 
+                    return newBoard
+    
